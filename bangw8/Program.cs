f0294d065a3de35b728318f70d307fe175a647d1
rayworld <casp@sina.com>
2019-05-14 09:59:26 +0800

    添加项目文件。

---
+++ bangw8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bangw8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
